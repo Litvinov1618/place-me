d99ebbc275a45d0b72c8f372a9d77b0629e9f667
Alex <50577816+Litvinov1618@users.noreply.github.com>
2020-08-10 14:52:47 +0300

    add PlaceCard.tsx content

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Header from './Header'
 import Navigation from './Navigation'
-import Container from '@material-ui/core/Container'
 import PlaceCard from './PlaceCard'
 import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
 
 const useStyles = makeStyles({
   root: {
--- src/components/PlaceCard.tsx
+++ src/components/PlaceCard.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardActions from '@material-ui/core/CardActions'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Divider from '@material-ui/core/Divider'
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    width: '344px',
+    margin: '0 auto 10px auto',
+  },
+  media: {
+    width: '136px',
+    height: '188px',
+  },
+  content: {
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+})
 
 interface PlaceCardProps {
   placeName: string
@@ -18,10 +41,38 @@ const PlaceCard: React.FC<PlaceCardProps> = ({
   dateRange,
   visitorsCount,
 }) => {
+  const classes = useStyles()
   return (
-    <Card>
-      <CardContent>Content</CardContent>
-      {isFree && <CardActions>Card Actions</CardActions>}
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        src="/img/user-avatar.png"
+      ></CardMedia>
+      <div className={classes.content}>
+        <CardContent>
+          <Typography variant="h5">{placeName}</Typography>
+          {!isFree ? (
+            <div>
+              <Typography color="textSecondary">{userEmail}</Typography>
+              <Typography color="textSecondary">{dateRange}</Typography>
+              <Typography color="textSecondary">
+                Visitors: {visitorsCount}
+              </Typography>
+            </div>
+          ) : (
+            <Typography color="textSecondary">Free</Typography>
+          )}
+        </CardContent>
+        {isFree && (
+          <div>
+            <Divider />
+            <CardActions>
+              <Button color="primary">Action</Button>
+              <Button color="primary">Action</Button>
+            </CardActions>
+          </div>
+        )}
+      </div>
     </Card>
   )
 }
